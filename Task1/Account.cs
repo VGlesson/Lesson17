6c864f30535b17adc35bb361625702c57ee14af9
Владимир Гаврилов <saxar.1kg@mail.ru>
2022-11-14 22:01:16 +0300

    Добавьте файлы проекта.

---
+++ Task1/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Account<T>
+    {
+        public T Num { get; set; }
+        public int Balanse { get; set; }
+        public string Name { get; set; }
+
+        //public Account(T num, int balanse, string name)
+        //{
+        //    Num = num;
+        //    Balanse = balanse;
+        //    Name = name;
+        //}
+
+        public string Getinfo()
+        {
+            return $"{Num} {Balanse} {Name}";
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета");
+            string stringNum = Console.ReadLine();
+            Num = (T)Convert.ChangeType(stringNum, typeof(T));
+
+            Console.WriteLine("Введите ФИО");
+            Name = Console.ReadLine();
+            Console.WriteLine("Ввведите баланс карты");
+            Balanse = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите номер счета");
+            //int numInt = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите ФИО");
+            //string name = Console.ReadLine();
+            //int balanse = Convert.ToInt32(Console.ReadLine());
+            //Account<int> account1 = new Account<int>(numInt, 100, name );
+            //Console.WriteLine(account1.Getinfo());
+
+            //Console.WriteLine("Введите строковый номер счета");
+            //string numString = Console.ReadLine();
+            //Console.WriteLine("Введите ФИО");
+            //name = Console.ReadLine();
+            //balanse = Convert.ToInt32(Console.ReadLine());
+            //Account<string> account2 = new Account<string>(numString, 200, name);
+            //Console.WriteLine(account2.Getinfo());
+
+            Account<int> account1 = new Account<int>();
+            account1.Input();
+            Console.WriteLine(account1.Getinfo());
+
+            Account<string> account2 = new Account<string>();
+            account2.Input();
+            Console.WriteLine(account2.Getinfo());
+
+            Console.ReadKey();
+        }
+    }
+}
